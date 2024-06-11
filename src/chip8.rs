83a1bec6666393ee25a713be72a3734646c3292c
Yuriy Senko <yuri.senko@capgemini.com>
2024-06-11 14:47:42 +0300

    Implement basic Chip8 system without display and input.

--- src/chip8.rs
+++ src/chip8.rs
@@ -5,3 +5,33 @@ mod opcodes;
 mod registers;
 mod stack;
 mod timers;
+
+pub struct Chip8 {
+    memory: memory::Memory,
+    registers: registers::Registers,
+    stack: stack::Stack,
+    timers: timers::Timers,
+}
+
+impl Chip8 {
+    pub fn new() -> Chip8 {
+        Chip8 {
+            memory: memory::Memory::new(),
+            registers: registers::Registers::new(),
+            stack: stack::Stack::new(),
+            timers: timers::Timers::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_chip8_new() {
+        Chip8::new();
+    }
+
+    // Add more test cases here...
+}
