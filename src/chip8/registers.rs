78660172d35b1786981869b9352174e36eb3aca9
Yuriy Senko <yuri.senko@capgemini.com>
2024-06-11 14:34:11 +0300

    Simplify registers implementation so data register idx can be passed as u8.

--- src/chip8/registers.rs
+++ src/chip8/registers.rs
@@ -1,54 +1,6 @@
-const DATA_REGISTER_COUNT: usize = 16;
+use std::convert::From;
 
-/// Represents the 16 data registers (V0 through VF) in Chip8.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum DataRegister {
-    V0,
-    V1,
-    V2,
-    V3,
-    V4,
-    V5,
-    V6,
-    V7,
-    V8,
-    V9,
-    VA,
-    VB,
-    VC,
-    VD,
-    VE,
-    VF,
-}
-
-impl DataRegister {
-    /// Converts the enum to its corresponding index in the data registers array.
-    pub fn to_index(self) -> usize {
-        self as usize
-    }
-    /// Converts the given index to its corresponding data register enum.
-    pub fn from_index(index: usize) -> Option<DataRegister> {
-        match index {
-            0 => Some(DataRegister::V0),
-            1 => Some(DataRegister::V1),
-            2 => Some(DataRegister::V2),
-            3 => Some(DataRegister::V3),
-            4 => Some(DataRegister::V4),
-            5 => Some(DataRegister::V5),
-            6 => Some(DataRegister::V6),
-            7 => Some(DataRegister::V7),
-            8 => Some(DataRegister::V8),
-            9 => Some(DataRegister::V9),
-            10 => Some(DataRegister::VA),
-            11 => Some(DataRegister::VB),
-            12 => Some(DataRegister::VC),
-            13 => Some(DataRegister::VD),
-            14 => Some(DataRegister::VE),
-            15 => Some(DataRegister::VF),
-            _ => None,
-        }
-    }
-}
+const DATA_REGISTER_COUNT: usize = 0xF;
 
 pub struct Registers {
     v: [u8; DATA_REGISTER_COUNT],
@@ -57,12 +9,20 @@ pub struct Registers {
 }
 
 impl Registers {
-    pub fn read_data_register(&self, register: DataRegister) -> u8 {
-        self.v[register.to_index()]
+    pub fn new() -> Registers {
+        Registers {
+            v: [0; DATA_REGISTER_COUNT],
+            i: 0,
+            pc: 0,
+        }
     }
 
-    pub fn write_data_register(&mut self, register: DataRegister, value: u8) {
-        self.v[register.to_index()] = value;
+    pub fn read_v(&self, register: u8) -> u8 {
+        self.v[register as usize]
+    }
+
+    pub fn write_v(&mut self, register: u8, value: u8) {
+        self.v[register as usize] = value;
     }
 }
 
@@ -71,45 +31,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_data_register_to_index() {
-        assert_eq!(DataRegister::V0.to_index(), 0);
-        assert_eq!(DataRegister::V1.to_index(), 1);
-        assert_eq!(DataRegister::V2.to_index(), 2);
-        assert_eq!(DataRegister::V3.to_index(), 3);
-        assert_eq!(DataRegister::V4.to_index(), 4);
-        assert_eq!(DataRegister::V5.to_index(), 5);
-        assert_eq!(DataRegister::V6.to_index(), 6);
-        assert_eq!(DataRegister::V7.to_index(), 7);
-        assert_eq!(DataRegister::V8.to_index(), 8);
-        assert_eq!(DataRegister::V9.to_index(), 9);
-        assert_eq!(DataRegister::VA.to_index(), 10);
-        assert_eq!(DataRegister::VB.to_index(), 11);
-        assert_eq!(DataRegister::VC.to_index(), 12);
-        assert_eq!(DataRegister::VD.to_index(), 13);
-        assert_eq!(DataRegister::VE.to_index(), 14);
-        assert_eq!(DataRegister::VF.to_index(), 15);
-    }
+    fn test_registers_new() {
+        let registers = Registers::new();
 
-    #[test]
-    fn test_data_register_from_index() {
-        assert_eq!(DataRegister::from_index(0), Some(DataRegister::V0));
-        assert_eq!(DataRegister::from_index(1), Some(DataRegister::V1));
-        assert_eq!(DataRegister::from_index(2), Some(DataRegister::V2));
-        assert_eq!(DataRegister::from_index(3), Some(DataRegister::V3));
-        assert_eq!(DataRegister::from_index(4), Some(DataRegister::V4));
-        assert_eq!(DataRegister::from_index(5), Some(DataRegister::V5));
-        assert_eq!(DataRegister::from_index(6), Some(DataRegister::V6));
-        assert_eq!(DataRegister::from_index(7), Some(DataRegister::V7));
-        assert_eq!(DataRegister::from_index(8), Some(DataRegister::V8));
-        assert_eq!(DataRegister::from_index(9), Some(DataRegister::V9));
-        assert_eq!(DataRegister::from_index(10), Some(DataRegister::VA));
-        assert_eq!(DataRegister::from_index(11), Some(DataRegister::VB));
-        assert_eq!(DataRegister::from_index(12), Some(DataRegister::VC));
-        assert_eq!(DataRegister::from_index(13), Some(DataRegister::VD));
-        assert_eq!(DataRegister::from_index(14), Some(DataRegister::VE));
-        assert_eq!(DataRegister::from_index(15), Some(DataRegister::VF));
-        assert_eq!(DataRegister::from_index(16), None);
-        assert_eq!(DataRegister::from_index(100), None);
+        assert_eq!(registers.v, [0; DATA_REGISTER_COUNT]);
+        assert_eq!(registers.i, 0);
+        assert_eq!(registers.pc, 0);
     }
 
     #[test]
@@ -120,12 +47,12 @@ mod tests {
             pc: 0,
         };
 
-        for index in 0..DATA_REGISTER_COUNT {
-            let register = DataRegister::from_index(index).unwrap();
+        for register_idx in 0..DATA_REGISTER_COUNT as u8 {
+            let register_value = register_idx;
 
-            registers.write_data_register(register, index as u8);
+            registers.write_v(register_idx, register_value);
 
-            assert_eq!(registers.read_data_register(register), index as u8);
+            assert_eq!(registers.read_v(register_idx), register_value);
         }
     }
 
